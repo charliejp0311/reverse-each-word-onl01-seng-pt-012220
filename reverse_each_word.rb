0aa36a339ebb0eec90fd059f4bbfd89f6c633544
Charles Pelton <charliejp0311@gmail.com>
2020-01-23 14:18:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,9 @@ def reverse_each_word(strng)
   words = []
   words = strng.split(" ")
   words.each do |word|
-    word = word.reverse
+    bw = ""
+    bw = word.reverse
+    pp bw
   end
+  words 
 end
